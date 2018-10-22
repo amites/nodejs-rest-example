8dca52da0602bc72363105347015039981eb5a63
Alvin Mites <alvin@mitesdesign.com>
2018-10-22 11:30:33 -0400

    Sample REST GET support

---
+++ cmds/console-commands-GET.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# setup git branch
+git fetch --all
+git branch --all
+git checkout 01-GET
+
+
+
+# cURL commands
+curl -X GET http://localhost:3000/api/users
+
+
+curl -X GET http://localhost:3000/api/user/1
+
+
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/api/users', (req, res) => {
     success: 'true',
     message: 'users retrieved successfully',
     users: dbUsers
-  });
+  })
 });
 
 // Define value to return for GET single user
@@ -39,64 +39,6 @@ app.get('/api/users/:userId', (req, res) => {
       user: {}
     });
   }
-});
-
-// Define value to return for POST users
-app.post('/api/users', (req, res) => {
-  let email = req.body.email;
-  if (!email) {
-    res.status(400).send({
-      success: 'false',
-      message: 'Did not receive an email address for new user',
-      user: {}
-    });
-  }
-
-  let newUser = {
-    id: dbUsers.length, // length is indexed from 1,
-    email: email
-  };
-  dbUsers.push(newUser);
-
-  return res.status(201).send({
-    success: 'true',
-    message: 'Created new user',
-    user: newUser
-  });
-});
-
-// Define value to return for PUT single user
-// PATCH not implemented since there is
-// only 1 value to update making PUT an equivalent
-app.put('/api/users/:userId', (req, res) => {
-  let userId = req.params.userId - 1; // shift 1 to left to match 0 based index
-
-  // Verify user is valid
-  if (!dbUsers[userId]) {
-    return res.status(404).send({
-      success: 'false',
-      message: `user with id ${userId + 1} does not exist`, // shift 1 to right to match original request
-      user: {}
-    });
-  }
-
-  // Verify data payload is valid
-  let email = req.body.email;
-  if (!email) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'Did not receive an email address for user',
-      user: {}
-    });
-  }
-
-  dbUsers[userId].email = email;
-
-  return res.status(200).send({
-    success: 'true',
-    message: 'Updated user successfully',
-    user: dbUsers[userId]
-  });
 });
 
 app.listen(port, () => {
